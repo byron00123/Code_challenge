71b93ce45ef68d7baaad3a7ba9df97bb31be3203
Byron Omondi <omondibyron25@gmail.com>
2023-03-20 17:27:43 +0300

    Completed assignment

--- README.md
+++ README.md
@@ -15,4 +15,36 @@ assign your output to the finalOutput variable
 
 
 # YOUR README DESCRIPTION STARTS HERE
+# Basic home Calculator
+   With simply their input, users of this program should be able to easily answer their math problems. Drivers, employees, and students can all use it to achieve the production of the desired results. For the marks they enter, the student grade generator outputs a grade. Based on the driver's speed, the speed detector determines the total amount of demerit points. Finaly, The goal of the salary calculator is to determine both the net wage and deductions.
+
+
+## studentgrade generator
+
+Students can input their marks
+The input will be from 0 to 100
+grade input will be outputed as follows
+    1. grade A > 79
+    2. grade B > 60 to 79
+    3. grade C > 59 to 49
+    4. grade D > 40 to 49
+    5. grade E < 40
+    
+## Speed detector
+The input will be the speed of the car and the output will be total number of demerit points
+  if speed < 70 km/hr it should print 'Ok'
+for every +5km/hr above 70 km/hr, one point is given
+And the output is total number of demerit points.
+If demerit points are more than 12, it should print 'License Suspendend'
+
+## Net salary calulator
+The input should be the basic salary and benefits given to an employee
+Program will calculate PAYE Tax,NHIF deductions, NSSF deductions and allow you to see the net salary
+
+
+
+
+## References
+[PAYEcalculator]https://www.kra.go.ke/individual/calculate-tax/calculating-tax/paye
+[Tax,Rates]https://www.aren.co.ke/payroll/taxrates.htm
 
--- script.js
+++ script.js
@@ -1,69 +1,128 @@
-let grade=document.getElementById('grade')
-let speed=document.getElementById('speed')
-let salary=document.getElementById('salary')
+let grade = document.getElementById("grade");
+let speed = document.getElementById("speed");
+let salary = document.getElementById("salary");
+
+grade.addEventListener("click", (e) => {
+  e.preventDefault();
+  let inputGrade = parseInt(document.getElementById("input-grade").value);
+  console.log(inputGrade);
+  let finalOutput = "marks";
+  // write Code For the Code challenge
+
+  if (inputGrade > 79) {
+    finalOutput = "A";
+  } else if (inputGrade >= 60) {
+    finalOutput = "B";
+  } else if (inputGrade >= 50) {
+    finalOutput = "C";
+  } else if (inputGrade >= 40) {
+    finalOutput = "D";
+  } else if (inputGrade <= 39) {
+    finalOutput = "E";
+  } else {
+    finalOutput = "Invalid Input";
+  }
+
+  console.log(finalOutput);
+
+  
+  document.getElementById("marks-id").append(finalOutput);
+});
+speed.addEventListener("click", (e) => {
+  e.preventDefault();
+  let inputSpeed = parseInt(document.getElementById("input-speed").value);
+  console.log(inputSpeed);
+  let finalOutput = "inputSpeed";
+  // write Code For the Code challenge
+
+  let demeritPoints = 0;
+
+  if (inputSpeed < 70) {
+    finalOutput = "Ok";
+  } else {
+    demeritPoints = Math.floor((inputSpeed - 70) / 5);
+    finalOutput = `Points: ${demeritPoints}`;
+
+    if (demeritPoints >= 12) {
+      finalOutput = "License suspended";
+    }
+  }
+
+  console.log(finalOutput);
+
+  
+  document.getElementById("speed-id").append(finalOutput);
+});
+salary.addEventListener("click", (e) => {
+    e.preventDefault();
+    let inputSalary = parseInt(document.getElementById("input-salary").value);
+    console.log(inputSalary);
+    let finalOutput;
+  // write Code For the Code challenge
+  //const prompt = require("prompt-sync")();
+
+const basicSalary = parseInt(prompt("Enter your basic salary: "));
+const benefits = parseInt(prompt("Enter your benefits: "));
+
+// KRA tax rates
+let payee;
+if (basicSalary + benefits <= 24000) {
+  payee = 0;
+} else if (basicSalary + benefits <= 40667) {
+  payee = (basicSalary + benefits - 24000) * 0.1;
+} else if (basicSalary + benefits <= 57733) {
+  payee = 1667 + (basicSalary + benefits - 40667) * 0.15;
+} else if (basicSalary + benefits <= 73333) {
+  payee = 4250 + (basicSalary + benefits - 57733) * 0.2;
+} else if (basicSalary + benefits <= 89667) {
+  payee = 8250 + (basicSalary + benefits - 73333) * 0.25;
+} else {
+  payee = 14050 + (basicSalary + benefits - 89667) * 0.3;
+}
+
+// NHIF rates
+let nhif;
+if (basicSalary + benefits < 6000) {
+  nhif = 150;
+} else if (basicSalary + benefits < 8000) {
+  nhif = 300;
+} else if (basicSalary + benefits < 12000) {
+  nhif = 400;
+} else if (basicSalary + benefits < 15000) {
+  nhif = 500;
+} else if (basicSalary + benefits < 20000) {
+  nhif = 600;
+} else if (basicSalary + benefits < 25000) {
+  nhif = 750;
+} else if (basicSalary + benefits < 30000) {
+  nhif = 850;
+} else if (basicSalary + benefits < 35000) {
+  nhif = 900;
+} else if (basicSalary + benefits < 40000) {
+  nhif = 950;
 
 
-grade.addEventListener('click', (e)=>{
-e.preventDefault()
-let inputGrade=parseInt(document.getElementById('input-grade').value)
-console.log(inputGrade)
-let finalOutput
-// write Code For the Code challenge
+} else {
+nhif = 1000;
+}
 
+// NSSF rates
+const nssf = basicSalary * 0.06;
 
+// Compute gross pay and net pay
+const grossPay = basicSalary + benefits;
+const totalDeductions = payee + nhif + nssf;
+const netPay = grossPay - totalDeductions;
 
-
-
-
-
-
-
-
-finalOutput='assign your final output to this variable'
-document.getElementById('marks-id').append(finalOutput)
-
-})
-speed.addEventListener('click', (e)=>{
-    e.preventDefault()
-    let inputSpeed=parseInt(document.getElementById('input-speed').value)
-    console.log(inputSpeed)
-    let finalOutput
-// write Code For the Code challenge
-
-
-
-
-
-
-
-
-
-
-
-
-
-finalOutput='assign your final output to this variable'
-    document.getElementById('speed-id').append(finalOutput)
-
-})
-salary.addEventListener('click', (e)=>{
-        e.preventDefault()
-        let inputSalary=parseInt(document.getElementById('input-salary').value)
-        console.log(inputSalary)
-let finalOutput
-// write Code For the Code challenge
-
-
-
-
-
-
-
-
-
-
-
-finalOutput ='assign your final output to this variable'
-document.getElementById('salary-id').append(finalOutput)
-        
-})
+// Display results
+console.log(`Basic Salary: ${basicSalary}`);
+console.log(`Benefits: ${benefits}`);
+console.log(`Gross Pay: ${grossPay}`);
+console.log(`Payee: ${payee}`);
+console.log(`NHIF: ${nhif}`);
+console.log(`NSSF: ${nssf}`);
+console.log(`Total Deductions: ${totalDeductions}`);
+console.log(`Net Pay: ${netPay}`);
+  
+  document.getElementById("salary-id").append(finalOutput);
+});
